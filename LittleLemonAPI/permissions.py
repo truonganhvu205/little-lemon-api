b4717fe55dfe7e7f2eb159bb9af6f785112c91ee
Truong Anh Vu <122198260+truonganhvu205@users.noreply.github.com>
2023-12-15 10:20:17 +0700

    Update permissions.py

--- LittleLemonAPI/permissions.py
+++ LittleLemonAPI/permissions.py
@@ -2,10 +2,10 @@ from rest_framework import permissions
 
 class IsManager(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.user.groups.filter(name='Manager').exists():
+        if request.user.groups.filter(name='manager').exists():
             return True
 
 class IsDeliveryCrew(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.user.groups.filter(name='Delivery crew').exists():
+        if request.user.groups.filter(name='delivery crew').exists():
             return True
